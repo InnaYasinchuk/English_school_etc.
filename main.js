97c6d523a834336fe4655471567849633219a03c
Inna Yasynchuk <i_bybyk@ukr.net>
2024-05-20 21:27:06 +0300

    created header

---
+++ main.js
@@ -0,0 +1,47 @@
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+document.addEventListener('DOMContentLoaded', function() {
+            document.getElementById('access_key').value = accessKey;
+        });
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    
+    // Очищення вмісту result перед відправкою нового запиту
+    result.innerHTML = "Please wait...";
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async(response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                // Зміна тексту повідомлення про успішне відправлення
+                result.innerHTML = "Your custom success message"; // Встановлення вашого власного тексту повідомлення
+               
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
+
