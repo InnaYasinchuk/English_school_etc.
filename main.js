5daa187ee561399c0e8ded5ac764fa52f436a844
Inna Yasynchuk <i_bybyk@ukr.net>
2024-05-25 16:26:55 +0300

    added video in teachers section

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-// SWIPER TEACHERS
+//  TEACHERS SWIPER
 
 const teachersSwiper = new Swiper("#teachersSwiper", {
   slidesPerView: 1.24,
@@ -15,7 +14,7 @@ const teachersSwiper = new Swiper("#teachersSwiper", {
     dynamicBullets: true,
     // dynamicMainBullets: 2,
   },
-   breakpoints: {
+  breakpoints: {
     360: {
       slidesPerView: 1.4,
       spaceBetween: 10,
@@ -23,81 +22,126 @@ const teachersSwiper = new Swiper("#teachersSwiper", {
     410: {
       slidesPerView: 1.5,
       spaceBetween: 15,
-     },
-    
+    },
+
     470: {
       slidesPerView: 1.6,
       spaceBetween: 15,
-     },
-    
+    },
+
     510: {
       slidesPerView: 1.9,
       spaceBetween: 15,
-     },
-    
+    },
+
     655: {
       slidesPerView: 2,
       spaceBetween: 15,
-     },
-    
-     860: {
+    },
+
+    860: {
       slidesPerView: 2.5,
       spaceBetween: 18,
-     },
-     992: {
+    },
+    992: {
       slidesPerView: 3,
       spaceBetween: 22,
     },
   },
 });
 
+// Modal
+
+let btns = document.querySelectorAll("*[data-modal-btn]");
+const body = document.body;
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let name = btns[i].getAttribute("data-modal-btn");
+    let modal = document.querySelector("*[data-modal-box = '" + name + "']");
+    modal.classList.add("show");
+    body.classList.add("lock");
+
+    let video = modal.querySelector("video");
+    if (video) {
+      video.play();
+    }
+
+    let close = modal.querySelector(".teachers-modal-box__close-btn");
+    close.addEventListener("click", function () {
+      modal.classList.remove("show");
+      body.classList.remove("lock");
+
+      let video = modal.querySelector("video");
+      if (video) {
+        video.pause();
+        video.currentTime = 0;
+      }
+    });
+  });
+}
+
+window.onclick = function (e) {
+  if (e.target.hasAttribute('data-modal-box')) {
+    let modals = document.querySelectorAll('*[data-modal-box]');
+    for (let i = 0; i < modals.length; i++) {
+      let modal = modals[i];
+      if (e.target === modal) {
+        modal.classList.remove("show");
+        body.classList.remove("lock");
+
+        let video = modal.querySelector("video");
+        if (video) {
+          video.pause();
+          video.currentTime = 0;
+        }
+      }
+    }
+  }
+}
 
 
 
+// CONTACT FORM
 
+const form = document.getElementById("form");
+const result = document.getElementById("result");
 
-// Contact Form
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
 
-const form = document.getElementById('form');
-const result = document.getElementById('result');
+  // Очищення вмісту result перед відправкою нового запиту
+  result.innerHTML = "Please wait...";
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const object = Object.fromEntries(formData);
-    const json = JSON.stringify(object);
-    
-    // Очищення вмісту result перед відправкою нового запиту
-    result.innerHTML = "Please wait...";
-
-    fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: json
-        })
-        .then(async(response) => {
-            let json = await response.json();
-            if (response.status == 200) {
-                // Зміна тексту повідомлення про успішне відправлення
-                result.innerHTML = "Your custom success message"; // Встановлення вашого власного тексту повідомлення
-               
-            } else {
-                console.log(response);
-                result.innerHTML = json.message;
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            result.innerHTML = "Something went wrong!";
-        })
-        .then(function() {
-            form.reset();
-            setTimeout(() => {
-                result.style.display = "none";
-            }, 3000);
-        });
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: json,
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        // Зміна тексту повідомлення про успішне відправлення
+        result.innerHTML = "Your custom success message"; // Встановлення вашого власного тексту повідомлення
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 3000);
+    });
 });
-
