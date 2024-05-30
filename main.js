925cc8407eb0db830e75b16a0334a7c88ce201a8
Inna Yasynchuk <i_bybyk@ukr.net>
2024-05-30 19:50:04 +0300

    added questions section

--- main.js
+++ main.js
@@ -103,6 +103,73 @@ window.onclick = function (e) {
   }
 };
 
+// QUESTIONS
+
+let accordionTitle = document.querySelectorAll(".accordion-item-title");
+
+
+accordionTitle.forEach((el, index) => {
+  el.addEventListener("click", () => {
+    let accordionContent = el.nextElementSibling;
+    let faqClose = document.querySelectorAll(".faq-close")[index];
+    let windowWidth = window.innerWidth;
+
+    if (accordionContent.style.maxHeight) {
+      document.querySelectorAll(".accordion-item-content").forEach((el) => {
+        el.style.maxHeight = null;
+        el.style.marginTop = "0px";
+      });
+      document.querySelectorAll(".faq-close").forEach((el) => {
+        el.style.transform = `scale(${getScaleValueFaqClose(windowWidth)}) rotate(0deg)`;
+        el.style.fill = "#007AFF";
+      });
+    } else {
+      document.querySelectorAll(".accordion-item-content").forEach((el) => {
+        el.style.maxHeight = null;
+        el.style.marginTop = "0px";
+      });
+      document.querySelectorAll(".faq-close").forEach((el) => {
+        el.style.transform = `scale(${getScaleValueFaqClose(windowWidth)}) rotate(0deg)`;
+        el.style.fill = "#007AFF";
+      });
+
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+      accordionContent.style.marginTop = `${getMarginTopValue(windowWidth)}px`;
+      faqClose.style.transform = `scale(${getScaleValueFaqClose(windowWidth)}) rotate(45deg)`;
+      faqClose.style.fill = "#F0BB75";
+    }
+  });
+});
+
+function getScaleValueFaqClose(width) {
+  if (width <= 389.98) {
+    return 0.45;
+  } else if (width > 389.98 && width <= 575.98) {
+    return 0.5;
+  } else if (width > 575.98 && width <= 767.98) {
+    return 0.6;
+  } else if (width > 767.98 && width <= 991.98) {
+    return 0.8;
+  } else {
+    return 1;
+  }
+}
+
+function getMarginTopValue(width) {
+  if (width <= 389.98) {
+    return 5;
+  } else if (width > 389.98 && width <= 575.98) {
+    return 8;
+  } else if (width > 575.98 && width <= 767.98) {
+    return 10;
+  } else if (width > 767.98 && width <= 991.98) {
+    return 15;
+  } else {
+    return 20;
+  }
+}
+
+
 // CONTACT FORM
 
 const form = document.getElementById("form");
